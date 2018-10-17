77c4b9df8952adaa0cd565a747b81e5166fc708e
Ran Bi <biran@mit.edu>
2018-10-17 03:42:02 +0200

    add option to specify output directory

--- validate_forest_branches.sh
+++ validate_forest_branches.sh
@@ -45,6 +45,8 @@ ARGS=()
 
 while [ $# -gt 0 ]; do
    case "$1" in 
+      -d|--dir)      dir="$2"; shift 2 ;;
+      --dir=*)       dir="${1#*=}"; shift ;;
       -n|--new)      newonly=1; shift ;;
       -r|--ref)      refonly=1; shift ;;
       -*|--*)        echo -e "invalid option: $1\n"; exit 1 ;;
@@ -60,9 +62,12 @@ set -- "${ARGS[@]}"
 }
 
 [[ $refonly || $newonly ]] && nargs=1 || nargs=2
+[ $dir ] && ((++nargs))
 [ $# -ne $nargs ] && { echo -e "check number of arguments\n"; exit 1; }
 
-[ $newonly ] && { new=$1; } || { ref=$1; new=$2; }
+[ $newonly ] && new=$1 || ref=$1
+[[ ! $refonly ]] && { new=$2; shift; }
+[ $dir ] && outdir=$2 || outdir=vfb-output
 
 # check cms environment
 if [[ -z $CMSSW_BASE || $(pwd)/ != $CMSSW_BASE/* ]]; then
@@ -80,13 +85,13 @@ fi
 
 # produce forest files
 [[ ! $newonly ]] && {
-   mkdir -p vfb-output/$ref/;
-   run_configs $ref vfb-output/$ref/;
+   mkdir -p $outdir/$ref/;
+   run_configs $ref $outdir/$ref/;
 }
 
 [[ ! $refonly ]] && {
-   mkdir -p vfb-output/$new/;
-   run_configs $new vfb-output/$new/;
+   mkdir -p $outdir/$new/;
+   run_configs $new $outdir/$new/;
 }
 
 popd
